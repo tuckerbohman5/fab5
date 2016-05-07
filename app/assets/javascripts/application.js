70465d3a3637c20649f7602ea8280f7a65669286
tuckerbohman5 <tucker.bohman@gmail.com>
2016-05-07 11:00:20 -0600

    created application.js file

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+//= require angular
+//= require angular-ui-router
+//= require angular-rails-templates
+// require lodash
+//= require angular-simple-logger
+//= require angular-google-maps
+//= require angular-devise
+//= require angular-resource
+//= require angular-sanitize
+//= require angular-messages
+
+// = require jquery
+//= require bootstrap-sprockets
+//= require_tree .
